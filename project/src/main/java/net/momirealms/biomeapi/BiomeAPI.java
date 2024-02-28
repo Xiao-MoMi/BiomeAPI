c101667c833c0e1a768516dbdde288c8992e84e0
XiaoMoMi <972454774@qq.com>
2024-02-29 00:01:21 +0800

    Update BiomeAPI.java

--- project/src/main/java/net/momirealms/biomeapi/BiomeAPI.java
+++ project/src/main/java/net/momirealms/biomeapi/BiomeAPI.java
@@ -21,35 +21,40 @@ public class BiomeAPI {
     }
 
     public String getBiome(Location location) {
-        if (customBiome == null) {
-            try {
-                Class.forName("net.minecraft.world.level.biome.BiomeBase");
-                switch (Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3]) {
-                    case "v1_20_R3" -> this.customBiome = new CustomBiome1_20_R3();
-                    case "v1_20_R2" -> this.customBiome = new CustomBiome1_20_R2();
-                    case "v1_20_R1" -> this.customBiome = new CustomBiome1_20_R1();
-                    case "v1_19_R3" -> this.customBiome = new CustomBiome1_19_R3();
-                    case "v1_19_R2" -> this.customBiome = new CustomBiome1_19_R2();
-                    case "v1_19_R1" -> this.customBiome = new CustomBiome1_19_R1();
-                    case "v1_18_R2" -> this.customBiome = new CustomBiome1_18_R2();
-                    case "v1_18_R1" -> this.customBiome = new CustomBiome1_18_R1();
-                    case "v1_17_R1" -> this.customBiome = new CustomBiome1_17_R1();
-                    default -> throw new UnsupportedVersionException();
-                }
-            } catch (ClassNotFoundException ignore) {
-                switch (Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3]) {
-                    case "v1_20_R3" -> this.customBiome = new CustomBiome1_20_R3_Mojmap();
-                    case "v1_20_R2" -> this.customBiome = new CustomBiome1_20_R2_Mojmap();
-                    case "v1_20_R1" -> this.customBiome = new CustomBiome1_20_R1_Mojmap();
-                    case "v1_19_R3" -> this.customBiome = new CustomBiome1_19_R3_Mojmap();
-                    case "v1_19_R2" -> this.customBiome = new CustomBiome1_19_R2_Mojmap();
-                    case "v1_19_R1" -> this.customBiome = new CustomBiome1_19_R1_Mojmap();
-                    case "v1_18_R2" -> this.customBiome = new CustomBiome1_18_R2_Mojmap();
-                    case "v1_18_R1" -> this.customBiome = new CustomBiome1_18_R1_Mojmap();
-                    default -> throw new UnsupportedVersionException();
-                }
-            }
-        }
+        if (customBiome == null)
+            init();
         return customBiome.getBiomeAt(location);
+    }
+
+    private void init() {
+        switch (Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3] + (isMojMap() ? "_Mojmap" : "")) {
+            case "v1_20_R3" -> this.customBiome = new CustomBiome1_20_R3();
+            case "v1_20_R2" -> this.customBiome = new CustomBiome1_20_R2();
+            case "v1_20_R1" -> this.customBiome = new CustomBiome1_20_R1();
+            case "v1_19_R3" -> this.customBiome = new CustomBiome1_19_R3();
+            case "v1_19_R2" -> this.customBiome = new CustomBiome1_19_R2();
+            case "v1_19_R1" -> this.customBiome = new CustomBiome1_19_R1();
+            case "v1_18_R2" -> this.customBiome = new CustomBiome1_18_R2();
+            case "v1_18_R1" -> this.customBiome = new CustomBiome1_18_R1();
+            case "v1_17_R1" -> this.customBiome = new CustomBiome1_17_R1();
+            case "v1_20_R3_Mojmap" -> this.customBiome = new CustomBiome1_20_R3_Mojmap();
+            case "v1_20_R2_Mojmap" -> this.customBiome = new CustomBiome1_20_R2_Mojmap();
+            case "v1_20_R1_Mojmap" -> this.customBiome = new CustomBiome1_20_R1_Mojmap();
+            case "v1_19_R3_Mojmap" -> this.customBiome = new CustomBiome1_19_R3_Mojmap();
+            case "v1_19_R2_Mojmap" -> this.customBiome = new CustomBiome1_19_R2_Mojmap();
+            case "v1_19_R1_Mojmap" -> this.customBiome = new CustomBiome1_19_R1_Mojmap();
+            case "v1_18_R2_Mojmap" -> this.customBiome = new CustomBiome1_18_R2_Mojmap();
+            case "v1_18_R1_Mojmap" -> this.customBiome = new CustomBiome1_18_R1_Mojmap();
+            default -> throw new UnsupportedVersionException();
+        }
+    }
+
+    private boolean isMojMap() {
+        try {
+            Class.forName("net.minecraft.world.level.biome.BiomeBase");
+            return false;
+        } catch (ClassNotFoundException ignore) {
+            return true;
+        }
     }
 }
