bc5f09fe2ee60088de18872c95b5c4f5a4e3f1a4
XiaoMoMi <972454774@qq.com>
2024-05-03 02:14:15 +0800

    Lazy init

--- common/src/main/java/net/momirealms/biomeapi/UnsupportedVersionException.java
+++ common/src/main/java/net/momirealms/biomeapi/UnsupportedVersionException.java
@@ -1,4 +1,12 @@
 package net.momirealms.biomeapi;
 
 public class UnsupportedVersionException extends RuntimeException {
+
+    public UnsupportedVersionException(String message) {
+        super(message);
+    }
+
+    public UnsupportedVersionException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- project/src/main/java/net/momirealms/biomeapi/BiomeAPI.java
+++ project/src/main/java/net/momirealms/biomeapi/BiomeAPI.java
@@ -6,9 +6,6 @@ import org.jetbrains.annotations.Nullable;
 
 public class BiomeAPI {
 
-    private static BiomeAPI api;
-    private CustomBiome customBiome;
-
     /**
      * The biome would be null if the server version is not supported
      *
@@ -17,48 +14,83 @@ public class BiomeAPI {
      */
     @Nullable
     public static String getBiomeAt(Location location) {
-        if (api == null) api = new BiomeAPI();
-        return api.getBiome(location);
+        return SingletonHolder.INSTANCE.getBiomeAt(location);
     }
 
-    public String getBiome(Location location) {
-        if (customBiome == null)
-            init();
-        return customBiome.getBiomeAt(location);
-    }
+    private static class SingletonHolder {
+        private static final CustomBiome INSTANCE = init();
 
-    private void init() {
-        String bukkitVersion = Bukkit.getServer().getBukkitVersion().split("-")[0];
-        switch (bukkitVersion + (isMojMap() ? "-Mojmap" : "")) {
-            case "1.20.5", "1.20.6" -> this.customBiome = new CustomBiome1_20_R4();
-            case "1.20.3", "1.20.4" -> this.customBiome = new CustomBiome1_20_R3();
-            case "1.20.2" -> this.customBiome = new CustomBiome1_20_R2();
-            case "1.20", "1.20.1" -> this.customBiome = new CustomBiome1_20_R1();
-            case "1.19.4" -> this.customBiome = new CustomBiome1_19_R3();
-            case "1.19.3" -> this.customBiome = new CustomBiome1_19_R2();
-            case "1.19", "1.19.1", "1.19.2" -> this.customBiome = new CustomBiome1_19_R1();
-            case "1.18.2" -> this.customBiome = new CustomBiome1_18_R2();
-            case "1.18", "1.18.1" -> this.customBiome = new CustomBiome1_18_R1();
-            case "1.17", "1.17.1" -> this.customBiome = new CustomBiome1_17_R1();
-            case "1.20.5-Mojmap", "1.20.6-Mojmap" -> this.customBiome = new CustomBiomeMojMapR1();
-            case "1.20.3-Mojmap", "1.20.4-Mojmap" -> this.customBiome = new CustomBiome1_20_R3_Mojmap();
-            case "1.20.2-Mojmap" -> this.customBiome = new CustomBiome1_20_R2_Mojmap();
-            case "1.20-Mojmap", "1.20.1-Mojmap" -> this.customBiome = new CustomBiome1_20_R1_Mojmap();
-            case "1.19.4-Mojmap" -> this.customBiome = new CustomBiome1_19_R3_Mojmap();
-            case "1.19.3-Mojmap" -> this.customBiome = new CustomBiome1_19_R2_Mojmap();
-            case "1.19-Mojmap", "1.19.1-Mojmap", "1.19.2-Mojmap" -> this.customBiome = new CustomBiome1_19_R1_Mojmap();
-            case "1.18.2-Mojmap" -> this.customBiome = new CustomBiome1_18_R2_Mojmap();
-            case "1.18-Mojmap", "1.18.1-Mojmap" -> this.customBiome = new CustomBiome1_18_R1_Mojmap();
-            default -> throw new UnsupportedVersionException();
+        private static CustomBiome init() {
+            String bukkitVersion = Bukkit.getServer().getBukkitVersion().split("-")[0];
+            switch (bukkitVersion + (isMojMap() ? "-Mojmap" : "")) {
+                case "1.20.5", "1.20.6" -> {
+                    return new CustomBiome1_20_R4();
+                }
+                case "1.20.3", "1.20.4" -> {
+                    return new CustomBiome1_20_R3();
+                }
+                case "1.20.2" -> {
+                    return new CustomBiome1_20_R2();
+                }
+                case "1.20", "1.20.1" -> {
+                    return new CustomBiome1_20_R1();
+                }
+                case "1.19.4" -> {
+                    return new CustomBiome1_19_R3();
+                }
+                case "1.19.3" -> {
+                    return new CustomBiome1_19_R2();
+                }
+                case "1.19", "1.19.1", "1.19.2" -> {
+                    return new CustomBiome1_19_R1();
+                }
+                case "1.18.2" -> {
+                    return new CustomBiome1_18_R2();
+                }
+                case "1.18", "1.18.1" -> {
+                    return new CustomBiome1_18_R1();
+                }
+                case "1.17", "1.17.1" -> {
+                    return new CustomBiome1_17_R1();
+                }
+                case "1.20.5-Mojmap", "1.20.6-Mojmap" -> {
+                    return new CustomBiomeMojMapR1();
+                }
+                case "1.20.3-Mojmap", "1.20.4-Mojmap" -> {
+                    return new CustomBiome1_20_R3_Mojmap();
+                }
+                case "1.20.2-Mojmap" -> {
+                    return new CustomBiome1_20_R2_Mojmap();
+                }
+                case "1.20-Mojmap", "1.20.1-Mojmap" -> {
+                    return new CustomBiome1_20_R1_Mojmap();
+                }
+                case "1.19.4-Mojmap" -> {
+                    return new CustomBiome1_19_R3_Mojmap();
+                }
+                case "1.19.3-Mojmap" -> {
+                    return new CustomBiome1_19_R2_Mojmap();
+                }
+                case "1.19-Mojmap", "1.19.1-Mojmap", "1.19.2-Mojmap" -> {
+                    return new CustomBiome1_19_R1_Mojmap();
+                }
+                case "1.18.2-Mojmap" -> {
+                    return new CustomBiome1_18_R2_Mojmap();
+                }
+                case "1.18-Mojmap", "1.18.1-Mojmap" -> {
+                    return new CustomBiome1_18_R1_Mojmap();
+                }
+                default -> throw new UnsupportedVersionException(bukkitVersion + (isMojMap() ? "-Mojmap" : ""));
+            }
         }
-    }
 
-    private boolean isMojMap() {
-        try {
-            Class.forName("net.minecraft.world.level.biome.BiomeBase");
-            return false;
-        } catch (ClassNotFoundException ignore) {
-            return true;
+        private static boolean isMojMap() {
+            try {
+                Class.forName("net.minecraft.world.level.biome.Biome");
+                return true;
+            } catch (ClassNotFoundException ignore) {
+                return false;
+            }
         }
     }
 }
